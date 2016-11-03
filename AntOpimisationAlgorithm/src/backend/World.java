6541ecf2a564bf2e9ee20209a65c62d2befbb116
Josh Jin <joshxjin@gmail.com>
2016-11-03 15:19:29 +1300

    Able to generate nodes with at least one link with neighbour nodes to create complete path from start to finish.

---
+++ AntOpimisationAlgorithm/src/backend/World.java
@@ -0,0 +1,79 @@
+package backend;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class World {
+	public ArrayList<Node> nodeList = new ArrayList<Node>();
+	
+	int worldWidth, worldHeight, numOfMiddleNodes;
+	GraphicsContext gc;
+	
+	Random r = new Random();
+	
+	public World(int canvasWidth, int canvasHeight, int numOfMiddleNodes, GraphicsContext gc) {
+		this.worldWidth = canvasWidth;
+		this.worldHeight = canvasHeight;
+		this.numOfMiddleNodes = numOfMiddleNodes;
+		this.gc = gc;
+	}
+	
+	
+	public void generateNodes() {
+		nodeList.clear();
+		Node startNode = new Node(50, 50, gc, "start");
+		nodeList.add(startNode);
+		Node endNode = new Node(worldWidth - 50, worldHeight - 50, gc, "end");
+		nodeList.add(endNode);
+		
+		for (int i = 0; i < numOfMiddleNodes; i++) {
+			generateNextNode("middle");
+		}
+		
+		generatePaths();
+	}
+	
+	private void generateNextNode(String type) {
+		int newX = r.nextInt(worldWidth - 60) + 30;
+		int newY = r.nextInt(worldHeight - 60) + 30;
+		Boolean validPosition = true;
+		for (Node node: nodeList) {
+			double distance = node.calculateDistance(newX, newY);
+			if (distance <= 150) {
+				validPosition = false;
+				break;
+			}
+		}
+		
+		if (!validPosition) {
+			generateNextNode(type);
+		} else {
+			Node newNode = new Node(newX, newY, gc, type);
+			nodeList.add(newNode);
+		}
+	}
+	
+	private void generatePaths() {
+		int linkDistance = 300;
+		for (int i = 0; i < nodeList.size(); i++) {
+			Node node = nodeList.get(i);
+			for (Node neighbourNode: nodeList) {
+				if (!neighbourNode.equals(node) && neighbourNode.calculateDistance(node.x, node.y) <= linkDistance && !neighbourNode.neighbours.contains(node)) {
+					node.neighbours.add(neighbourNode);
+					neighbourNode.neighbours.add(node);
+					gc.setFill(Color.BLACK);
+					gc.strokeLine(node.x + 15, node.y + 15, neighbourNode.x + 15, neighbourNode.y + 15);
+				}
+			}
+			if (node.neighbours.isEmpty()) {
+				linkDistance += 50;
+				i--;
+			} else {
+				linkDistance = 300;
+			}
+		}
+	}
+}
--- AntOpimisationAlgorithm/src/frontend/App.java
+++ AntOpimisationAlgorithm/src/frontend/App.java
@@ -4,43 +4,44 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import backend.Node;
+import backend.World;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
 	final private int CANVAS_WIDTH = 900;
-	final private int CANVAS_HEIGHT = 700;
+	final private int CANVAS_HEIGHT = 800;
 	final private int RIGHT_PANEL_WIDTH = 200;
 	final private int MIDDLE_NODES_NUMBER = 15;
+	
 	private GraphicsContext gc;
 	private Random r = new Random();
-	private ArrayList<Node> nodeList = new ArrayList<Node>();
+	private World world;
 	
 	@Override
 	public void start(Stage primaryStage) {
-
+		
         BorderPane root = new BorderPane(); //root is a BorderPane
         
         VBox rightPanel = new VBox(); //right panel (buttons) is VBox
         setupRightPanel(rightPanel);
         root.setRight(rightPanel);
         
-        
         Canvas centerPanel = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT); //center panel is Canvas
         gc = centerPanel.getGraphicsContext2D();
+        world = new World(CANVAS_WIDTH, CANVAS_HEIGHT, MIDDLE_NODES_NUMBER, gc);
         setupCenterPanel();
         root.setCenter(centerPanel);
+        
 
         Scene scene = new Scene(root, CANVAS_WIDTH + RIGHT_PANEL_WIDTH, CANVAS_HEIGHT);
 
@@ -70,35 +71,7 @@ public class App extends Application {
 	
 	private void generateNodes() {
 		setupCenterPanel();
-		nodeList.clear();
-		Node startNode = new Node(50, 50, gc, "start");
-		nodeList.add(startNode);
-		Node endNode = new Node(CANVAS_WIDTH - 50, CANVAS_HEIGHT - 50, gc, "end");
-		nodeList.add(endNode);
-		
-		for (int i = 0; i < MIDDLE_NODES_NUMBER; i++) {
-			generateNextNode("middle");
-		}
-	}
-	
-	private void generateNextNode(String type) {
-		int newX = r.nextInt(CANVAS_WIDTH - 60) + 30;
-		int newY = r.nextInt(CANVAS_HEIGHT - 60) + 30;
-		Boolean validPosition = true;
-		for (Node node: nodeList) {
-			double distance = node.calculateDistance(newX, newY);
-			if (distance <= 150) {
-				validPosition = false;
-				break;
-			}
-		}
-		
-		if (!validPosition) {
-			generateNextNode(type);
-		} else {
-			Node newNode = new Node(newX, newY, gc, type);
-			nodeList.add(newNode);
-		}
+		world.generateNodes();
 	}
 
 	public static void main(String[] args) {
